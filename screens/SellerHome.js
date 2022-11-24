acbb569ef9ceec2aca1dd48dc22d07c3306197b9
Mohammad Abdur Rahman <abdur8606@gmail.com>
2022-11-24 20:22:13 +0800

    bottom tabs fixed

--- screens/SellerHome.js
+++ screens/SellerHome.js
@@ -7,53 +7,57 @@ import ItemCatalog from "../components/sellerHome/ItemCatalog";
 
 export default function SellerHome() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <ScrollView>
-        <HomeHeader />
-        <View>
-          <Text
-            style={{
-              fontFamily: "Roboto",
-              fontSize: 18,
-              fontWeight: "bold",
-              marginLeft: "5%",
-              marginTop: "5%",
-            }}
-          >
-            Looking for a protein {'\n'}rich diet?
-          </Text>
+        <View style={{ flex: 1 }}>
+          <HomeHeader />
+          <View>
+            <Text
+              style={{
+                fontFamily: "Roboto",
+                fontSize: 18,
+                fontWeight: "bold",
+                marginLeft: "5%",
+                marginTop: "5%",
+              }}
+            >
+              Looking for a protein {"\n"}rich diet?
+            </Text>
+          </View>
+          <ItemSlider />
+          <View>
+            <Text
+              style={{
+                fontFamily: "Roboto",
+                fontSize: 18,
+                fontWeight: "bold",
+                marginLeft: "5%",
+                marginTop: "5%",
+              }}
+            >
+              Looking for a protein {"\n"}rich diet?
+            </Text>
+          </View>
+          <ItemSlider />
+          <View>
+            <Text
+              style={{
+                fontFamily: "Roboto",
+                fontSize: 18,
+                fontWeight: "bold",
+                marginLeft: "5%",
+                marginTop: "5%",
+              }}
+            >
+              Popular Products For You
+            </Text>
+          </View>
+          <ItemCatalog />
         </View>
-        <ItemSlider />
-        <View>
-          <Text
-            style={{
-              fontFamily: "Roboto",
-              fontSize: 18,
-              fontWeight: "bold",
-              marginLeft: "5%",
-              marginTop: "5%",
-            }}
-          >
-            Looking for a protein {'\n'}rich diet?
-          </Text>
-        </View>
-        <ItemSlider />
-        <View>
-          <Text
-            style={{
-              fontFamily: "Roboto",
-              fontSize: 18,
-              fontWeight: "bold",
-              marginLeft: "5%",
-              marginTop: "5%",
-            }}
-          >
-            Popular Products For You
-          </Text>
-        </View>
-        <ItemCatalog />
-        <BottomTabs />
       </ScrollView>
+      <View>
+        <BottomTabs />
+      </View>
     </View>
   );
 }
