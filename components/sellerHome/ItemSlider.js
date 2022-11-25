85bbb24e57860e0c8ac6f5975c3625e5fef0a0f6
Mohammad Abdur Rahman <abdur8606@gmail.com>
2022-11-25 17:05:14 +0800

    products data read and display - home buyer seller

--- components/sellerHome/ItemSlider.js
+++ components/sellerHome/ItemSlider.js
@@ -1,46 +1,43 @@
 import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SellerItem from "./SellerItem";
-
-const items = [
-  {
-    name: "Chicken legs",
-    image:
-      "https://thumbs.dreamstime.com/b/raw-chicken-drumstick-white-background-raw-chicken-drumstick-193150228.jpg",
-    price: "60",
-    ordersPlaced: "100",
-    imageHeight: 250,
-    imageWidth: 250,
-  },
-  {
-    name: "Beef",
-    image:
-      "https://media.istockphoto.com/id/505207430/photo/fresh-raw-beef-steak.jpg?s=612x612&w=0&k=20&c=QxOege3Io4h1TNJLtGYh71rxb29p1BfFcZvCipz4WVY=",
-    price: "80",
-    ordersPlaced: "100",
-    imageHeight: 250,
-    imageWidth: 250,
-  },
-  {
-    name: "Lamb Chops",
-    image:
-      "https://media.istockphoto.com/id/105489252/photo/rack-of-lamb-with-rosemary.jpg?s=612x612&w=0&k=20&c=O1Vpfvq9_TZOsuy0QdaXyx3sFOvH3kGf29nPBZB_aHY=",
-    price: "100",
-    ordersPlaced: "100",
-    imageHeight: 250,
-    imageWidth: 250,
-  },
-  {
-    name: "Eggs",
-    image: "https://static.toiimg.com/photo/msid-68521300/68521300.jpg",
-    price: "20",
-    ordersPlaced: "100",
-    imageHeight: 250,
-    imageWidth: 250,
-  },
-];
+import {firebase} from '../../config';
 
 export default function ItemSlider() {
+  const [items, setItems] = useState([]);
+  const productsRef = firebase.firestore().collection("products");
+
+  useEffect(async () => {
+    productsRef.onSnapshot(
+      (querySnapshot) => {
+        const newItems = [];
+        querySnapshot.forEach((doc) => {
+          const {
+            image,
+            price,
+            productName,
+            quantityAvailable,
+            quantitySold,
+            seller,
+          } = doc.data();
+          newItems.push({
+            id: doc.id,
+            image,
+            price,
+            productName,
+            quantityAvailable,
+            quantitySold,
+            seller,
+          });
+        });
+        setItems(newItems);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
   return (
     <>
       <ScrollView horizontal>
@@ -54,12 +51,12 @@ export default function ItemSlider() {
               style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
             >
               <SellerItem
-                name={item.name}
+                name={item.productName}
                 price={item.price}
-                ordersPlaced={item.ordersPlaced}
+                ordersPlaced={item.quantitySold}
                 image={item.image}
-                imageHeight={item.imageHeight}
-                imageWidth={item.imageWidth}
+                imageHeight={250}
+                imageWidth={300}
               />
             </View>
           </TouchableOpacity>
